43edba23b5e6f927b79bc445fa49fda41d94e658
G E M Y <75592828+gemyhex@users.noreply.github.com>
2021-03-31 06:30:08 +0200

    fix navbar

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,20 +1,32 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $(".slider-wrapper .carousel .carousel-inner .carousel-item").css({
-        'height': ($(window).height()) + 'px'
+        height: $(window).height() + "px",
     });
 
-    $('.movies-wrap .shuffle-items .m-item .page_two').parent().parent().hide();
-    $('.movies-wrap .shuffle-items .m-item .page_three').parent().parent().hide();
-    $('.movies-wrap .shuffle-items .m-item .page_four').parent().parent().hide();
-    $('.shuffle-list ul li').on('click' , function(){
-        $(this).addClass('active').siblings().removeClass('active');
-        if($(this).data('class') === '.page_one'){
-            $('.shuffle-items .m-item').hide();
-            $($(this).data('class')).parent().parent().show();
-        }else{
-            $('.shuffle-items .m-item').hide();
-            $($(this).data('class')).parent().parent().show();
-
+    $(".movies-wrap .shuffle-items .m-item .page_two").parent().parent().hide();
+    $(".movies-wrap .shuffle-items .m-item .page_three")
+        .parent()
+        .parent()
+        .hide();
+    $(".movies-wrap .shuffle-items .m-item .page_four")
+        .parent()
+        .parent()
+        .hide();
+    $(".shuffle-list ul li").on("click", function () {
+        $(this).addClass("active").siblings().removeClass("active");
+        if ($(this).data("class") === ".page_one") {
+            $(".shuffle-items .m-item").hide();
+            $($(this).data("class")).parent().parent().show();
+        } else {
+            $(".shuffle-items .m-item").hide();
+            $($(this).data("class")).parent().parent().show();
         }
     });
 });
+
+function openMenu() {
+    $(".side-wrapper").css("width" , "75%");
+}
+function closeMenu() {
+    $(".side-wrapper").css("width" , "0");
+}
